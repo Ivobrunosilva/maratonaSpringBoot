d1b0f20f3a3b3c7dfc1438f2d910be60929cec40
ivobruno <ivobruno@alunos.ifce.edu.br>
2020-01-04 19:31:22 -0300

    subindo comentario

--- src/main/java/br/com/ivo/awesome/AplicationStart.java
+++ src/main/java/br/com/ivo/awesome/AplicationStart.java
@@ -3,6 +3,7 @@ package br.com.ivo.awesome;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/*autor ivo */
 @SpringBootApplication
 public class AplicationStart {
 	public static void main(String[] args) {
