53b6d99dc85f244aba560b7eced2c9ea3635c1d8
Ivo Bruno da Silva <31710731+Ivobrunosilva@users.noreply.github.com>
2020-01-06 08:38:31 -0300

    salvando metodo post com funcionalidade

--- src/main/java/br/com/ivo/awesome/endpoint/StudentEndpoint.java
+++ src/main/java/br/com/ivo/awesome/endpoint/StudentEndpoint.java
@@ -1,6 +1,5 @@
 package br.com.ivo.awesome.endpoint;
 
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,16 +17,16 @@ import br.com.ivo.awesome.util.DateUtil;
 @RequestMapping("student")
 public class StudentEndpoint {
 	private DateUtil dateUtil;
-	
+
 	@Autowired
 	public StudentEndpoint(DateUtil dateUtil) {
 		super();
 		this.dateUtil = dateUtil;
 	}
-	
+
 	@RequestMapping(method = RequestMethod.GET)
 	public ResponseEntity<?> listAll() {
-		return new ResponseEntity<>(Student.studentList,HttpStatus.OK);
+		return new ResponseEntity<>(Student.studentList, HttpStatus.OK);
 	}
 
 	@RequestMapping(method = RequestMethod.GET, path = "/{id}")
@@ -35,14 +34,14 @@ public class StudentEndpoint {
 		Student student = new Student();
 		student.setId(id);
 		int index = Student.studentList.indexOf(student);
-		if(index == -1)
+		if (index == -1)
 			return new ResponseEntity<>(new CustomErrorType("Estudante não encontrado"), HttpStatus.NOT_FOUND);
-		return new ResponseEntity<>(Student.studentList.get(index),HttpStatus.OK);
+		return new ResponseEntity<>(Student.studentList.get(index), HttpStatus.OK);
 	}
-	
-	@RequestMapping(method = RequestMethod.POST)
+	/*lembrar da diferença entre post e get...*/
+	@RequestMapping(value = "/save", method = RequestMethod.POST)
 	public ResponseEntity<?> save(@RequestBody Student student) {
 		Student.studentList.add(student);
-		return new ResponseEntity<>(student,HttpStatus.OK);
+		return new ResponseEntity<>(student, HttpStatus.OK);
 	}
 }
