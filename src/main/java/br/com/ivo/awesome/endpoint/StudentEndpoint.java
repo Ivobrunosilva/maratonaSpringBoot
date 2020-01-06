6df235d5875dfb6b27d6e41a1aea5395c83edaa6
Ivo Bruno da Silva <31710731+Ivobrunosilva@users.noreply.github.com>
2020-01-06 17:02:23 -0300

    add delete e put e deixando a notação simplificada

--- src/main/java/br/com/ivo/awesome/endpoint/StudentEndpoint.java
+++ src/main/java/br/com/ivo/awesome/endpoint/StudentEndpoint.java
@@ -1,9 +1,11 @@
 package br.com.ivo.awesome.endpoint;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -11,18 +13,10 @@ import org.springframework.web.bind.annotation.RestController;
 
 import br.com.ivo.awesome.error.CustomErrorType;
 import br.com.ivo.awesome.model.Student;
-import br.com.ivo.awesome.util.DateUtil;
 
 @RestController
 @RequestMapping("student")
 public class StudentEndpoint {
-	private DateUtil dateUtil;
-
-	@Autowired
-	public StudentEndpoint(DateUtil dateUtil) {
-		super();
-		this.dateUtil = dateUtil;
-	}
 
 	@RequestMapping(method = RequestMethod.GET)
 	public ResponseEntity<?> listAll() {
@@ -38,10 +32,21 @@ public class StudentEndpoint {
 			return new ResponseEntity<>(new CustomErrorType("Estudante não encontrado"), HttpStatus.NOT_FOUND);
 		return new ResponseEntity<>(Student.studentList.get(index), HttpStatus.OK);
 	}
-	/*lembrar da diferença entre post e get...*/
-	@RequestMapping(value = "/save", method = RequestMethod.POST)
+	@PostMapping
 	public ResponseEntity<?> save(@RequestBody Student student) {
 		Student.studentList.add(student);
 		return new ResponseEntity<>(student, HttpStatus.OK);
 	}
+	@DeleteMapping
+	public ResponseEntity<?> delete(@RequestBody Student student) {
+		Student.studentList.remove(student);
+		return new ResponseEntity<>(student, HttpStatus.OK);
+	}
+	@PutMapping
+	public ResponseEntity<?> update(@RequestBody Student student) {
+		Student.studentList.remove(student);
+		Student.studentList.add(student);
+		return new ResponseEntity<>(HttpStatus.OK);
+	}
+	
 }
